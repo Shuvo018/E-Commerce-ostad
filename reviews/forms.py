3a7bbcc88d22c69237b611428fb2ed831bd933ae
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-06-30 00:43:45 +0600

    created: review views

---
+++ reviews/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Review
+
+class ReviewForm(forms.ModelForm):
+    rating = forms.IntegerField(min_value=1, max_value=5)
+
+    class Meta:
+        model = Review
+        fields = ['rating', 'common']
+        widgets = {
+            'comment': forms.Textarea(attrs={'rows': 4})
+        }
--- reviews/views.py
+++ reviews/views.py
@@ -1,2 +1,55 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from products.models import Product, ProductVariant
+from .models import Review
+from accounts.models import CustomerProfile
+from reviews.forms import ReviewForm
+
+# Create your views here.
+@login_required
+def review_create(request, product_slug):
+    product = get_object_or_404(Product, slug=product_slug)
+    customer = get_object_or_404(CustomerProfile, user=request.user)
+    if Review.objects.filter(customer=customer, product=product).exists():
+        messages.error(request, 'You have already reviewed this product')
+        return redirect('product_detail', pk=product.pk)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.customer = customer
+            review.product = product
+            review.save()
+            messages.success(request, 'Review submitted.')
+            return redirect('product_detail',  pk=product.pk)
+    else:
+        form = ReviewForm()
+    return render(request, 'reviews/review_form.html', {'form': form})
 
 
+@login_required
+def review_update(request, pk):
+    customer = get_object_or_404(CustomerProfile, user=request.user)
+    review = get_object_or_404(Review, pk=pk, customer=customer)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Review updated')
+            return redirect('product_detail', pk=review.product.pk)
+    else:
+        form = ReviewForm(instance=review)
+    return render(request, 'reviews/review_form.html', {'form': form})
+
+@login_required
+def review_delete(request, pk):
+    customer = get_object_or_404(CustomerProfile, user=request.user)
+    review = get_object_or_404(Review, pk=pk, customer=customer)
+    if request.method == 'POST':
+        product_id= review.product.pk
+        review.delete()
+        messages.success(request, 'Review deleted.')
+        return redirect('product_detail', pk=product_id)
+    return render(request, 'reviews/review_confirm_delete.html')
+    
